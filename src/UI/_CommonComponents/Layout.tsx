d14154489e6f65545afd5392845a3096cf44d2c3
panzerkunst <1530772+PanzerKunst@users.noreply.github.com>
2026-03-26 09:16:06 +0100

    Removed AppHeader, AppFooter, and added normalize.css lib

--- src/UI/_CommonComponents/Layout.tsx
+++ src/UI/_CommonComponents/Layout.tsx
@@ -1,16 +1,11 @@
 import { Outlet, ScrollRestoration } from "react-router-dom"
 
-import { AppFooter } from "./AppFooter.tsx"
-import { AppHeader } from "./AppHeader/AppHeader.tsx"
-
 import "./Layout.scss"
 
 export function Layout() {
   return (
     <>
-      <AppHeader/>
       <Outlet />
-      <AppFooter/>
       <ScrollRestoration />
     </>
   )
